b8bc108d581c17885fbcc7a181c581f6ea072466
Ajmal2025 <139298518+Ajmal2025@users.noreply.github.com>
2023-10-25 21:58:03 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,26 @@
+// 14.	Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+
+let guestList: Array <string>= [
+
+"Rafiq",
+"Iqbal",
+"Babar"
+];
+
+guestList.forEach((guestName) =>{
+
+
+    console.log(`Dear ${guestName}, your are invited to a dinner tonight at my place.`)
+})
+
+
+
+
+
+
+
+
+
+
+
